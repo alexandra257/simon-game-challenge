caad880e8cd41c71783cfd1396f020d66e3ec564
Alexandra <alexandrajn17@gmail.com>
2020-05-07 16:51:41 +0100

    play relevant sound file & making code dynamic
    dynamically selected the sound file by concatenating the file name and using new Audio to generate the audio object. also updated the if statement so that the id is selected with jQuery dynamically

--- game.js
+++ game.js
@@ -1,23 +1,18 @@
 const buttonColours = ["red", "blue", "green", "yellow"];
+
 const gamePattern = [];
 
 function nextSequence() {
   var randomNum = Math.floor(Math.random() * 4); //generating random num between 0-3
   const randomChosenColour = buttonColours[randomNum]; //getting the colour at the index that matches the random number
-  //   console.log(randomNum); //checking that the colour matched the number/index generatod
-  console.log(randomChosenColour);
 
-  if (randomChosenColour === "red") {
-    //if the red button is chosen, blink it
-    $("#red").fadeOut(500).fadeIn(500);
-  }
+  gamePattern.push(randomChosenColour); //adding the random colour generated onto the end of the gamePattern array
 
-  return gamePattern.push(randomChosenColour); //adding the random colour generated onto the end of the gamePattern array
+  $(`#${randomChosenColour}`) //concatenating the id so it matches whatever random colour is generated
+    .fadeIn(100)
+    .fadeOut(100)
+    .fadeIn(100);
+
+  var audio = new Audio(`sounds/${randomChosenColour}.mp3`); //concatenating the audio file name so it matches whatever random colour is generated
+  audio.play();
 }
-
-console.log(nextSequence());
-console.log(gamePattern);
-// console.log(nextSequence());
-// console.log(gamePattern);
-// console.log(nextSequence());
-// console.log(gamePattern);
